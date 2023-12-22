5af4314dbaa99a29b7a1919a2f7927a52d9954ab
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-22 19:39:25 +0530

    Created user model

---
+++ api/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique: true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},{timestamps: true});
+
+// timestamps for getting the time of creating and updation of the user
+
+// creating the user model
+const User = mongoose.model('User',userSchema);
+
+export default User;
