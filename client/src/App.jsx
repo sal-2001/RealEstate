4c144adc8ed6a83c3f82c4463d6fce2ea01e2c93
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-22 16:55:47 +0530

    header created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./pages/components/Header";
 
 export default function App() {
   return (
-    <div className='text-center'>App</div>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
