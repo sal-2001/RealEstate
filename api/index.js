5903927eb1a3b210ea00f91bddb092d82304ffd0
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-22 19:23:14 +0530

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 300");
+  console.log("Server is running on port 3000!");
 });
