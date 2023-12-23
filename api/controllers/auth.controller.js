803331e6ef3fcc3eb4cdde882564f2ad68e9d194
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-23 15:22:47 +0530

    created middleware and function for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,23 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async(req,res)=>{
-    const {username,email, password} = req.body;
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    //hash the password => we don't need to write await in front as it is already using await by default
-    //the second value (10 here) is the salt number (number of round for creating the salt)
-    const hashedPassword = bcrypt.hashSync(password,10);
-    // creating a new user with given details
-    const newUser = new User({username,email,password: hashedPassword});
+  //hash the password => we don't need to write await in front as it is already using await by default
+  //the second value (10 here) is the salt number (number of round for creating the salt)
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  // creating a new user with given details
+  const newUser = new User({ username, email, password: hashedPassword });
 
-    // for deplaying the error while authentication, we use try and catch method
-    try{
-        //save the new user in the database
+  // for deplaying the error while authentication, we use try and catch method
+  try {
+    //save the new user in the database
 
     await newUser.save();
     res.status(201).json("User created successfully!");
-    }catch(error){
-        // to see the error in the insomnia, instead of the terminal
-        res.status(500).json(error.message);
-    }
-    
+  } catch (error) {
+    // to see the error in the insomnia, instead of the terminal
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -23,5 +23,17 @@ app.listen(3000, () => {
 // this allows json as the input of the server
 app.use(express.json());
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+
+//middleware for handling errors
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+});
