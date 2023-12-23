c2023a42213c61f3ccad3d13682e96c2934d59bd
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-24 00:55:29 +0530

    Completed signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+//everytime the fetching link has /api init then direct it to localhost:3000
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': {
+        target: "http://127.0.0.1:3000",
+        secure:false,
+      },
+    },
+  },
   plugins: [react()],
 })
