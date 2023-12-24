86625aefb5ec04ab8b1a262bf6d5ddd4d656f373
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-24 12:04:51 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -18,6 +20,37 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully!");
   } catch (error) {
     // to see the error in the insomnia, instead of the terminal
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    //if email is valid or not
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    } else {
+      //matching the password
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+
+      if (!validPassword) {
+        return next(errorHandler(401, "Wrong credentials!"));
+      } else {
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        //we want to remove the password data from the token, so we destructure the user details, ie, we save everything but the password
+        const { password: pass, ...rest } = validUser._doc;
+
+        // we can add expiry date for our cookie like (expire: new Date() + 24*60*60*100) means will expire after 1000 days
+        res
+          .cookie("access_token", token, { httpOnly: true })
+          .status(200)
+          .json(rest);
+      }
+    }
+  } catch (error) {
     next(error);
   }
 };
