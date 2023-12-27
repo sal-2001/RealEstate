2fb0c5252616ec7dbca2073b47a82093dceb88e2
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-28 02:09:14 +0530

    completed create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import {
   getDownloadURL,
   getStorage,
@@ -182,6 +183,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          to={"/create-listing"}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create Listing
+        </Link>
       </form>
       {error && <p className="text-red-500 mt-5 text-center">{error}</p>}
       {updateSuccess && (
