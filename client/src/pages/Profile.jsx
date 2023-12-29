83ada4612c8d14af3b20e7a59eb49ab3edf69da4
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2023-12-29 19:29:20 +0530

    completed delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -138,12 +138,27 @@ export default function Profile() {
         setShowListingError(true);
         return;
       }
-      console.log(data);
       setUserListings(data);
       setShowListingError(false);
     } catch (error) {
       setShowListingError(true);
       setShowListingLoading(false);
+    }
+  };
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
       console.log(error);
     }
   };
@@ -260,7 +275,12 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  className="text-red-700 uppercase"
+                  onClick={() => handleDeleteListing(listing._id)}
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
